a63b0d313a919e3cc96c282088990fd311fee94b
root <root@LAPTOP-2H5HCFUQ.localdomain>
2020-06-30 17:12:46 -0400

    Added common folder

--- cnn_block_maxTs/parse.py
+++ cnn_block_maxTs/parse.py
@@ -6,11 +6,19 @@ lines = f.readlines()
 w = open('bin/block_result.csv', 'a')
 writer = csv.writer(w)
 
+result = []
+result.append(int(sys.argv[1]))
+result.append(int(sys.argv[2]))
+result.append(int(sys.argv[3]))
+
 for line in lines:
     if len(line.split()) != 0 and line.split()[0] == 'Kernel':
-        gflops = 16 * 16 * 128 * 128 * 4 * 4 * 2 / (float(line.split(' ')[4]) / 1000) / 1000000000
-        writer.writerow([sys.argv[1], sys.argv[2], sys.argv[3], line.split(' ')[4], str(gflops)])
-        break
+        result.append(float(line.split(' ')[4]))
+
+gflops = 16 * 16 * 128 * 128 * 4 * 4 * 2 / (min(result[3:]) / 1000) / 1000000000
+result.append(min(result[3:]))
+result.append(gflops)
+writer.writerow(result)
 
 f.close()
 w.close()
--- cnn_block_maxTs/run.sh
+++ cnn_block_maxTs/run.sh
@@ -5,15 +5,20 @@ Tr=$2
 Tc=$3
 Tn=1
 
+echo "Tm,Tr,Tc,Test#1,test#2,test#3,test#4,test#5,test#6,test#7,test#8,test#9,test#10,MIN Kernel Time,GFLOPS" > bin/block_result.csv
 for (( i=4; i<=$Tm; i++ ))
 do
     for (( j=4; j<=$Tr; j++ ))
     do
         for (( k=4; k<=$Tc; k++ ))
         do
-                echo $i $j $k
-                ./bin/host $i $j $k $Tn > bin/temp
-                python parse.py $i $j $k $Tn
+            for (( m=1; m<=10; m++))
+            do
+                echo $i $j $k $m
+                ./bin/host $i $j $k $Tn >> bin/temp
+            done
+            python parse.py $i $j $k $Tn
+            rm bin/temp
         done
     done
 done
